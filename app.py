f9806887d3477534cd5ae2684db00faf8c8eb714
edwardlhwang <elhwang24@gmail.com>
2023-07-14 09:27:27 -0700

    Adding Youtube API connection

--- app.py
+++ app.py
@@ -1,5 +1,7 @@
 import os
+import requests
 import openai
+import re
 from flask import Flask, redirect, render_template, request, url_for
 
 app = Flask(__name__)
@@ -8,33 +10,79 @@ openai.api_key = os.getenv("OPENAI_API_KEY")
 
 @app.route("/", methods=("GET", "POST"))
 def index():
+
+
     if request.method == "POST":
-        news_url = request.form["url"]
-        human = request.form["human"]
+        videoLink = request.form["videoLink"]
+        videoId = get_video_id(videoLink)
+        videoComments = get_youtube_comments(videoId)
+        print(videoComments)
         response = openai.Completion.create(
-        model="text-davinci-003",
-        prompt=generate_prompt(news_url),
-        temperature=0.6
+            model="text-davinci-003",
+            prompt=generate_prompt("test", videoComments),
+            temperature=0.6,
         )
-        response2 = openai.Completion.create(
-        model="text-davinci-003",
-        prompt=generate_prompt(human),
-        temperature=0.6
-        )
-
-        combinedGPTResponse = "Animal: " +  response.choices[0].text +  "\n\n Human:" + response2.choices[0].text
-        return redirect(url_for("index", result=combinedGPTResponse))
-
+        return redirect(url_for("index", result=response.choices[0].text))
+    
     result = request.args.get("result")
     return render_template("index.html", result=result)
 
 
-def generate_prompt(animal):
-    return """Suggest three names for an animal that is a superhero.
+def generate_prompt(animal, videoComments):
+    # for comment in videoComments:
+    #    print(comment)
+    return """Given the list of youtube comments, provide a rating from 0-100, with 0 being misinformation and bad content, and 100 being stellar entertaining content with good information {}
+Names:""".format(
+        animal.capitalize()
+    )
 
-Animal: Cat
-Names: Captain Sharpclaw, Agent Fluffball, The Incredible Feline
-Animal: Dog
-Names: Ruff the Protector, Wonder Canine, Sir Barks-a-Lot
-Animal: {}
-Names:""".format(animal.capitalize())
+
+
+def get_youtube_comments(video_id):
+  """Gets all the comments on a YouTube video.
+
+  Args:
+    video_id: The ID of the YouTube video.
+
+  Returns:
+    A list of dictionaries, where each dictionary represents a comment.
+  """
+
+  api_key = os.getenv("YOUTUBE_API_KEY")
+  url = "https://www.googleapis.com/youtube/v3/commentThreads"
+  params = {
+    "part": "snippet",
+    "videoId": video_id,
+    "maxResults": 100,
+  }
+
+  headers = {
+    "Authorization": f"Bearer {api_key}",
+  }
+
+  response = requests.get(url, params=params, headers=headers)
+  if response.status_code == 200:
+    data = response.json()
+    comments = []
+    for comment_thread in data["items"]:
+      comments.append(comment_thread["snippet"]["topLevelComment"]["snippet"])
+    return comments
+  else:
+    print(f"Error getting comments: {response.status_code}")
+    return None
+  
+def get_video_id(link):
+  """Gets the video ID from a YouTube link.
+
+  Args:
+    link: The YouTube link.
+
+  Returns:
+    The video ID.
+  """
+
+  match = re.search(r"watch\?v=(.*)", link)
+  if match:
+    return match.group(1)
+  else:
+    return None
